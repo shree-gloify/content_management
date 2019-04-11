193243506b196b7f57a03f7d5185e3b2d1261642
shree <shree@gloify.com>
2019-04-11 15:55:06 +0530

    updated swagger

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  before_action :set_frontend_root_url
+
+  def set_frontend_root_url
+    @frontend_root_url = Rails.application.config.frontend_site_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	
-  # mount Rswag::Ui::Engine => '/api-docs'
-  # mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   mount_devise_token_auth_for 'User', at: 'auth'
   root to: 'home#show'
 
